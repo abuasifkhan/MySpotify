dfd4fd99e85b16dd3877fb41154d63b5f5fec7f5
Abu Asif Khan Chowdhury <abu.chowdhury@selise.ch>
2018-02-05 19:49:07 +0100

    not hardcoded urls and 404 error shortcut: https://www.youtube.com/watch?v=vhBxt9T4Zvk

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
 from .models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -13,8 +12,9 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(id=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    # try:
+    #     album = Album.objects.get(id=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
