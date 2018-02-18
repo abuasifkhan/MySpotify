4f00dc6a051bdae51af3e8f5cbe3d8350b80c0db
Abu Asif Khan Chowdhury <abuasifkhan@MacBook-Pro.lan>
2018-02-18 17:22:42 +0100

    simple rest get request implemented

--- website/urls.py
+++ website/urls.py
@@ -2,11 +2,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
+from companies import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
+    url(r'^stocks/*', views.StockList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
 
 # In not production mode
 if settings.DEBUG:
